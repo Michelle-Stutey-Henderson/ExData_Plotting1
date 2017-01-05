ae59e8184c703702d2ba7bf7592ee4d5e82b601a
Michelle-Stutey-Henderson <michelle.s.henderson.99@gmail.com>
2017-01-04 18:31:41 -0800

    added R code and png for 4 plots

---
+++ Plot1.R
@@ -0,0 +1,41 @@
+###################################################
+## 1.3.2017 
+## Exploratory Analysis Project 1 - Part 1 of 4
+## UC Irvine Data set: Electric power consumption
+## Reproduce a histogram of Global Active Power
+## Create plot1.png
+## Restrict Dates to: 2007-02-01 and 2007-02-02
+## Note: missing data is coded as ? in dataset
+###################################################
+
+## Get data
+
+fileloc <- "./exdata_data_household_power_consumption/household_power_consumption.txt"
+readLines(fileloc,10)
+## Note: setting column class to "date" expects a certain ordering "year/month/day"
+##      however the data is set up as "day/month/year".  can use read.zoo to set date format, 
+##      but chose to import as character and convert to date
+columnClasses <- c("character","character","numeric","numeric","numeric","numeric","numeric", "numeric","numeric")
+EPCdata <- read.table(fileloc, header = TRUE, sep = ";", na.strings = "?", colClasses = columnClasses)
+
+##Data checks
+summary(EPCdata)
+colSums(is.na(EPCdata))
+table(EPCdata$Date)
+
+## subset data on dates: 2007-02-01 & 2007-02-02
+## "Date" is still character vector, will convert after subsetting 
+## current format: "d/m/yyyy"
+EPC <- EPCdata[EPCdata$Date == "1/2/2007"|EPCdata$Date == "2/2/2007", ]
+## add datetime; not necessary for plot 1 but needed for the others
+EPC$Datetime <- strptime(paste(EPC$Date,EPC$Time, sep =" "),format = "%d/%m/%Y %H:%M:%S")
+
+## Open graphical device: png 
+png(filename = "plot1.png")
+
+## Recreate histogram
+hist(EPC$Global_active_power, col = "red", main = "Global Active Power", 
+     xlab = "Global Active Power (kilowats)", ylab = "Frequency")
+
+## Close graphics device
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,47 @@
+###################################################
+## 1.3.2017 
+## Exploratory Analysis Project 1 - Part 3 of 4
+## UC Irvine Data set: Electric power consumption
+## Reproduce a histogram of Global Active Power
+## Create plot3.png
+## Restrict Dates to: 2007-02-01 and 2007-02-02
+## Note: missing data is coded as ? in dataset
+###################################################
+
+## Get data
+
+fileloc <- "./exdata_data_household_power_consumption/household_power_consumption.txt"
+readLines(fileloc,10)
+## Note: setting column class to "date" expects a certain ordering "year/month/day"
+##      however the data is set up as "day/month/year".  can use read.zoo to set date format, 
+##      but chose to import as character and convert to date
+columnClasses <- c("character","character","numeric","numeric","numeric","numeric","numeric", "numeric","numeric")
+EPCdata <- read.table(fileloc, header = TRUE, sep = ";", na.strings = "?", colClasses = columnClasses)
+
+##Data checks
+summary(EPCdata)
+colSums(is.na(EPCdata))
+table(EPCdata$Date)
+
+## subset data on dates: 2007-02-01 & 2007-02-02
+## "Date" is still character vector, will convert after subsetting 
+## current format: "d/m/yyyy"
+EPC <- EPCdata[EPCdata$Date == "1/2/2007"|EPCdata$Date == "2/2/2007", ]
+## add datetime; not necessary for plot 1 but needed for the others
+EPC$Datetime <- strptime(paste(EPC$Date,EPC$Time, sep =" "),format = "%d/%m/%Y %H:%M:%S")
+
+## Open graphical device: png 
+png(filename = "plot3.png")
+
+## Recreate graph
+with(EPC, plot(Datetime,Sub_metering_1, type = "n", xlab = "",
+               ylab = "Energy sub metering"))
+with(EPC, points(Datetime, Sub_metering_1, type = "l"))
+with(EPC, points(Datetime, Sub_metering_2, type = "l", col = "red"))
+with(EPC, points(Datetime, Sub_metering_3, type = "l", col = "blue"))
+legend("topright", lty = 1,col = c("black","red","blue"), 
+    legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+
+## Close graphics device
+dev.off()
